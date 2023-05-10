dca7eb955b89551bbb417d13e04b7804d5671d6d
bebeto123 <bebeto123@mail.ru>
2023-05-10 11:01:14 +0300

    create styles, add bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import './Styles/global.scss';
 
 export const App = () => {
-    return <div className="App">Learn React</div>;
+    return (
+        <div className="container">
+            Learn React
+            <br />
+            <button className="btn btn-outline-secondary">
+                <i className="bi bi-alarm"></i> Кнопка из bootstrap
+            </button>
+            <button className="btn btn-secondary">
+                <i className="bi bi-alarm"></i> Кнопка из bootstrap
+            </button>
+            <button className="btn btn-danger">
+                <i className="bi bi-exclamation-triangle-fill"></i> Кнопка из bootstrap
+            </button>
+            <button className="btn btn-outline-danger">
+                <i className="bi bi-exclamation-triangle-fill"></i> Кнопка из bootstrap
+            </button>
+        </div>
+    );
 };
