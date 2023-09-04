fb4d42b73060198e926d37766c293321c9b09ff7
bebeto-honot <bebeto123@mail.ru>
2023-09-04 20:56:33 +0300

    react-buddy

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { App } from 'App';
 import { setupStore } from 'Store';
+import { DevSupport } from '@react-buddy/ide-toolbox';
+import { ComponentPreviews, useInitial } from 'dev';
 
 const store = setupStore();
 
@@ -12,7 +14,9 @@ const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
     <BrowserRouter>
         <Provider store={store}>
-            <App />
+            <DevSupport ComponentPreviews={ComponentPreviews} useInitialHook={useInitial}>
+                <App />
+            </DevSupport>
         </Provider>
     </BrowserRouter>
 );
